cc7e98a232fca69690735cc02a90d3c0c3f75ec7
Perslay <natalia.44.fedyk@gmail.com>
2024-01-25 22:29:55 +0100

    Task 1 - gallery
    Adding elements to gallery.
    Using lightbox library to make pretty modal window after clicking on image. Added window closing after pressing escape key

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+const listItems = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li><div
+      class="gallery__item"><a
+      class="gallery__link" href=${original}></div>
+      <img class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt="${description}"
+      /></a></div></li>`
+  )
+  .join("");
+
+list.insertAdjacentHTML("afterbegin", listItems);
+
+const links = document.querySelectorAll(".gallery__link");
+
+links.forEach((link) => {
+  link.addEventListener("click", showModal);
+});
+
+function showModal(event) {
+  event.preventDefault();
+  const src = event.currentTarget.getAttribute("href");
+  const lightbox = basicLightbox.create(`<img src=${src} />`);
+  lightbox.show();
+
+  const escapeLightbox = (event) => {
+    if (event.key === "Escape") {
+      lightbox.close();
+      document.removeEventListener("keydown", escapeLightbox);
+    }
+  };
+
+  document.addEventListener("keydown", escapeLightbox);
+}
