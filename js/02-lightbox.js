3e28402172175b493e7d60dbcb2c8a253af3aa00
Perslay <natalia.44.fedyk@gmail.com>
2024-01-26 16:55:52 +0100

    Task 2 - second gallery
    Using simpleLightbox library instead of basicLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const list = document.querySelector(".gallery");
+
+const listItems = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li><a class="gallery__item" href=${original}>
+      <img class="gallery__image" src=${preview} alt="${description}" />
+      </a></li>`
+  )
+  .join("");
+
+list.insertAdjacentHTML("afterbegin", listItems);
+
+document.querySelector;
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
 
 console.log(galleryItems);
